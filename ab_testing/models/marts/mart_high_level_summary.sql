2187803b9bab86621b8dd90125096a775362b400
ajibola-komo <ajibolaokomolafe@gmail.com>
2026-07-25 21:10:29 +0100

    updated

--- ab_testing/models/marts/mart_high_level_summary.sql
+++ ab_testing/models/marts/mart_high_level_summary.sql
@@ -32,9 +32,11 @@ count(case when is_converted = 1 and test_group = 'control' and test_page = 'old
 nullif(count(case when test_group = 'control' and test_page = 'old_page' then user_id end),0)
 ) as absolute_lift,
 
+-- control traffic allocation
 count(case when test_group = 'control' and test_page = 'old_page' then user_id end)::FLOAT/
 nullif(count(user_id),0) as control_traffic_allocation,
 
+-- treatment traffic allocation
 count(case when test_group = 'treatment' and test_page = 'new_page' then user_id end)::FLOAT/
 nullif(count(user_id),0) as treatment_traffic_allocation
 
